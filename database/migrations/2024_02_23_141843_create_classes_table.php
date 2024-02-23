895fb0a2181aec78a7012ba5491674a3eed7808a
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-02-23 17:26:24 +0100

    create classed table

--- database/migrations/2024_02_23_141843_create_classes_table.php
+++ database/migrations/2024_02_23_141843_create_classes_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('classes', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->foreignId('teacher_id')->constrained('teachers')->onDelete('cascade')->onUpdate('cascade');
             $table->timestamps();
         });
     }
