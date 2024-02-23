19d904a53f36946696f8487cdf840e1f60bc8ac3
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-02-23 17:09:39 +0100

    create classes table

--- database/migrations/2024_02_23_141843_create_classes_table.php
+++ database/migrations/2024_02_23_141843_create_classes_table.php
@@ -11,9 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('skill_user', function (Blueprint $table) {
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade')->onUpdate('cascade');
-            $table->foreignId('skill_id')->constrained('skills')->onDelete('cascade')->onUpdate('cascade');
+        Schema::create('classes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
             $table->timestamps();
         });
     }
@@ -23,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('skill_user');
+        Schema::dropIfExists('classes');
     }
 };
