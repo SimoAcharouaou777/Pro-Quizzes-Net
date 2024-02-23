9fe8d58aa9e03593c20fbf7e3872fc9f28842b8e
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-02-23 17:08:16 +0100

    create company model

---
+++ app/Models/Company.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Company extends Model
+{
+    use HasFactory;
+    protected $table = 'companies';
+
+    protected $fillable = [
+        'company_name',
+        'company_email',
+        'description',
+        'domaine',
+        'location',
+        'founded_date',
+    ];
+
+    public function representatives()
+    {
+        return $this->hasOne(Representative::class);
+    }
+}
