aedac34a469f79788b0694c8f16fe53ee4350bb4
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-03-01 22:04:22 +0100

    create quize model

---
+++ app/Models/Quize.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Quize extends Model
+{
+    use HasFactory;
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
+    public function teachers()
+    {
+        return $this->belongsTo(Teacher::class);
+    }
+    public function classes()
+    {
+        return $this->belongsToMany(MyClass::class);
+    }
+    public function companies()
+    {
+        return $this->belongsTo(Company::class);
+    }
+    public function questions()
+    {
+        return $this->hasMany(Question::class);
+    }
+}
