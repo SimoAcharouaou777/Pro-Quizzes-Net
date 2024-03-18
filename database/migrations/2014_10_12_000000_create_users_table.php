cb5cb85c33339a5f7203c2de5efe90cf01c7fc8a
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-03-18 16:58:09 +0000

    Update users table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('fullname');
-            $table->string('headline');
+            $table->string('username');
+            $table->string('headline')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
