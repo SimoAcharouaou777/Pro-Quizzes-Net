0a8f7d76d3823162eb181d2321c7940ea6d97867
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-03-20 15:23:27 +0000

    Add AdminController with CRUD operations for managing users and roles

---
+++ app/Http/Controllers/Admin/AdminController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Organizer;
+use App\Models\Role;
+use App\Models\Teacher;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $users = User::all();
+        $roles = Role::all();
+        return view('admin.dashboard', compact('users','roles'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $user = User::find($id);
+        $request->validate([
+            'role' => 'required|in:admin,user,student,teacher, representative',
+        ]);
+        $roleName = $request->input('role');
+        $role = Role::where('name', $roleName)->first(); 
+        $user->roles()->sync([$role->id]);
+        return redirect(route('admin.index'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
