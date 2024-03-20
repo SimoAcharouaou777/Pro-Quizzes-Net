f941d8c9f78a90220b3ce2d6846aecb33d6112f4
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-03-20 15:24:30 +0000

    Update 'teachers' table column name to 'username'

--- database/migrations/2024_02_22_161157_create_teachers_table.php
+++ database/migrations/2024_02_22_161157_create_teachers_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('teachers', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username');
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade')->onUpdate('cascade');
             $table->timestamps();
         });
