ebbb691742a335a22d4b42f85087c23c1adf4370
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-03-20 15:23:50 +0000

    Update Teacher model's fillable fields

--- app/Models/Teacher.php
+++ app/Models/Teacher.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Teacher extends Model
 {
     use HasFactory;
-    protected $fillable = ['name','user_id']; 
+    protected $fillable = ['username','user_id']; 
 
     public function classes()
     {
