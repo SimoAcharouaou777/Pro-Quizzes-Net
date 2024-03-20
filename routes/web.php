23886ae7446318a40218280009b849d88f8d18d9
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-03-20 15:25:09 +0000

    Add admin dashboard route

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ForgetPasswordController;
-use App\Http\Controllers\ForgotPasswordController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,6 +26,7 @@ Route::get('/home',[HomeController::class, 'index'])->name('home');
 // handle view for user registration
 Route::get('/login', [AuthController::class, 'login'])->name('login');
 Route::get('/register', [AuthController::class, 'register'])->name('register');
+Route::get('logout', [AuthController::class, 'logout'])->name('logout');
 Route::get('/CompanyRegister', [AuthController::class, 'CompanyRegister'])->name('CompanyRegister');
 // handle user authentication
 Route::post('store', [AuthController::class, 'store'])->name('store');
@@ -37,3 +38,5 @@ Route::get('/forgot-password', [ForgetPasswordController::class, 'index'])->name
 Route::post('/forgot-password', [ForgetPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
 Route::get('/reset-password/{token}', [ForgetPasswordController::class, 'showResetForm'])->name('password.reset');
 Route::post('/reset-password', [ForgetPasswordController::class, 'reset'])->name('password.update');
+// admin dashboard
+Route::resource('admin', AdminController::class);
