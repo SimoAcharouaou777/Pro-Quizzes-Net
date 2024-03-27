3e2c77d51e9d620f897bbff59deff00590593448
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-03-27 12:32:44 +0000

    Add UserSettingsController with CRUD operations for user settings

---
+++ app/Http/Controllers/User/UserSettingsController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+
+class UserSettingsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $user = auth()->user();
+        return view('users.UserSettings', compact('user'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $request->validate([
+            'current_password' => 'required',
+            'password' => 'required|confirmed',
+        ]);
+    
+        $user = User::find($id);
+    
+        if (Hash::check($request->current_password, $user->password)) {
+            $user->password = Hash::make($request->password);
+            $user->save();
+
+            return back()->with('success', 'Password updated successfully');
+        } else {
+            return back()->withErrors(['current_password' => 'Current password does not match']);
+        }
+        
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $user = User::find($id);
+        $user->roles()->detach();
+        $user->delete();
+        return redirect(route('register'))->with('status', 'your account has been deleted successfully.');
+    }
+}
