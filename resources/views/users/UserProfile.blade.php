d6585d307885e6484cbedcaae3f1b38ea718f445
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-03-27 00:23:38 +0000

    Update user profile page with dynamic username and roles

--- resources/views/users/UserProfile.blade.php
+++ resources/views/users/UserProfile.blade.php
@@ -100,7 +100,7 @@
                             <a class="nav-link dropdown-toggle waves-effect waves-dark profile-pic" href=""
                                 data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img
                                     src="{{asset('assets/img/clients/profile.jpg')}}" alt="user" class="" /> <span
-                                    class="hidden-md-down">User name &nbsp;</span> </a>
+                                    class="hidden-md-down">{{$user->username}} &nbsp;</span> </a>
                             <ul class="dropdown-menu" aria-labelledby="navbarDropdown"></ul>
                         </li>
                     </ul>
@@ -119,10 +119,10 @@
                 <!-- Sidebar navigation-->
                 <nav class="sidebar-nav">
                     <ul id="sidebarnav">
-                        <li> <a class="waves-effect waves-dark" href="index.html" aria-expanded="false"><i
+                        <li> <a class="waves-effect waves-dark" href="{{route('userdashboard.index')}}" aria-expanded="false"><i
                                     class="fa fa-tachometer"></i><span class="hide-menu">Dashboard</span></a>
                         </li>
-                        <li> <a class="waves-effect waves-dark" href="pages-profile.html" aria-expanded="false"><i
+                        <li> <a class="waves-effect waves-dark" href="{{route('userprofile.index')}}" aria-expanded="false"><i
                                     class="fa fa-user-circle-o"></i><span class="hide-menu">Profile</span></a>
                         </li>
                         <li> <a class="waves-effect waves-dark" href="table-basic.html" aria-expanded="false"><i
@@ -181,10 +181,12 @@
                     <div class="col-lg-4 col-xlg-3 col-md-5">
                         <div class="card">
                             <div class="card-body">
-                                <center class="mt-4"> <img src="../assets/images/users/5.jpg" class="img-circle"
+                                <center class="mt-4"> <img src="{{asset('assets/images/users/5.jpg')}}" class="img-circle"
                                         width="150" />
-                                    <h4 class="card-title mt-2">Hanna Gover</h4>
-                                    <h6 class="card-subtitle">Accoubts Manager Amix corp</h6>
+                                    <h4 class="card-title mt-2">{{$user->username}}</h4>
+                                    @foreach($user->roles as $role)
+                                    <h6 class="card-subtitle">{{$role->name}}</h6>
+                                    @endforeach
                                     <div class="row text-center justify-content-md-center">
                                         <div class="col-4"><a href="javascript:void(0)" class="link"><i
                                                     class="fa fa-user"></i>
@@ -209,14 +211,14 @@
                                     <div class="form-group">
                                         <label class="col-md-12">Full Name</label>
                                         <div class="col-md-12">
-                                            <input type="text" placeholder="Johnathan Doe"
+                                            <input type="text" placeholder="place your full name here"
                                                 class="form-control form-control-line">
                                         </div>
                                     </div>
                                     <div class="form-group">
                                         <label for="example-email" class="col-md-12">Email</label>
                                         <div class="col-md-12">
-                                            <input type="email" placeholder="johnathan@admin.com"
+                                            <input type="email" placeholder="place your email here"
                                                 class="form-control form-control-line" name="example-email"
                                                 id="example-email">
                                         </div>
@@ -231,7 +233,7 @@
                                     <div class="form-group">
                                         <label class="col-md-12">Phone No</label>
                                         <div class="col-md-12">
-                                            <input type="text" placeholder="123 456 7890"
+                                            <input type="text" placeholder="place your phone number here"
                                                 class="form-control form-control-line">
                                         </div>
                                     </div>
