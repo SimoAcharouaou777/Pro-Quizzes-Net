a70c3eca382ef1cc36c93b6de121e43e1243e052
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-03-29 15:38:51 +0000

    Add table name and fillable fields to Question model

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -8,7 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Question extends Model
 {
     use HasFactory;
-
+    protected $table = 'questions';
+    protected $fillable = ['question', 'quize_id'];
     public function quizzes()
     {
         return $this->belongsTo(Quize::class);
