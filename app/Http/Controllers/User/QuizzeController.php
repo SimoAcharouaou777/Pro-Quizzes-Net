759c3f6884c3f0e491f311c99fc298725436cdf4
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-03-29 15:38:38 +0000

    Add models and implement quiz creation functionality

--- app/Http/Controllers/User/QuizzeController.php
+++ app/Http/Controllers/User/QuizzeController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\Answer;
+use App\Models\Question;
+use App\Models\Quize;
 use Illuminate\Http\Request;
 
 class QuizzeController extends Controller
@@ -13,7 +16,6 @@ class QuizzeController extends Controller
     public function index()
     {
         $user = auth()->user();
-        
         return view('users.UserQuizzes', compact('user'));
     }
 
@@ -30,7 +32,35 @@ class QuizzeController extends Controller
      */
     public function store(Request $request)
     {
-        //
+       $QuizeData = $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+
+        ]);
+        $quiz = Quize::create($QuizeData);
+
+        foreach($request->input('questions', []) as $index => $question) {
+            $request->validate([
+                'questions.{$index}.text' => 'required',
+                'questions.{$index}.choices' => 'required|array|min:3',
+                'questions.{$index}.choices.*' => 'required|integer',
+                'questions.{$index}.correct_answer'=>'required|array',
+                'questions.{$index}.correct_answer.*'=>'required|integer|in:'.implode(',', $question['choices']),
+            ]);
+        
+        $question = Question::creat([
+            'question' => $question['text'],
+            'quize_id' => $quiz->id,
+        ]);
+
+        foreach($question['choices'] as $choiceIndex => $choiceText){
+        $choice = Answer::create([
+            'response' => $choiceText,
+            'question_id' => $question->id,
+            'status' => in_array($choiceIndex, $question['correct_answer']) ? 'true' : 'false',
+    ]);
+        }
+      }
     }
 
     /**
