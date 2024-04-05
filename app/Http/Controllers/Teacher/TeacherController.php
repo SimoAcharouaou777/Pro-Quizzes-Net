26424afe52c3115f6b4196bac74f492eec1833c9
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-04-05 16:08:52 +0000

    Add index method to TeacherController and generate unique class code

--- app/Http/Controllers/Teacher/TeacherController.php
+++ app/Http/Controllers/Teacher/TeacherController.php
@@ -6,9 +6,16 @@ use App\Http\Controllers\Controller;
 use App\Models\MyClass;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 
 class TeacherController extends Controller
 {
+    public function index(){
+        $user = Auth::user();
+        $classes = auth()->user()->teacher->classes;
+        return view('users.teacher.TeacherClass', compact('user', 'classes'));
+    }
+    
     public function addClass(Request $request){
         $user = Auth::user();
         
@@ -18,14 +25,16 @@ class TeacherController extends Controller
             'level' => 'required',
             'learners' => 'required',
             'campus' => 'required',
-            'class_code' => 'required',
         ]);
         $data['teacher_id'] = $user->teacher->id;
         $data['teacher_name'] = $user->username;
-
+        do{
+            $class_code =rand(100000, 999999);
+        }while(Myclass::where('class_code', $class_code)->exists());
+        $data['class_code'] = $class_code;
         $class = MyClass::create($data);
         if ($request->hasFile('image')) {
-            $user->addMediaFromRequest('image')->toMediaCollection('media/classes', 'media_classes');
+            $class->addMediaFromRequest('image')->toMediaCollection('media/classes', 'media_classes');
         }
     }
 }
