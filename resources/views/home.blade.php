cdac041378c117c9ee202e99e0b68d09ec87f137
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-04-08 12:02:26 +0000

    Update class image size and learner count in TeacherClass and ClassDetails views

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,51 +1,137 @@
-@extends('layouts.app')
-@section('content')
-    
-    <div>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur 
-        <br> illum labore natus sint quam <br>
-        
-        odit, velit est adipisci culpa fugiat et quos animi necessitatibus quia laudantium impedit dolor. Minus, consectetur?
+<!DOCTYPE html>
+<html lang="en">
 
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Et dolorum amet ipsum quam nulla cupiditate quos magni enim adipisci temporibus? Earum pariatur et quis dignissimos dolores magni voluptatibus. Vero, suscipit?
-        Ipsa, distinctio? Dolor recusandae a, mollitia officiis incidunt perspiciatis. Enim architecto vel nobis dolores nemo aspernatur qui voluptatum blanditiis officia consectetur, consequatur temporibus quam ex sunt modi tempora officiis quas.
-        Expedita labore ducimus pariatur earum a voluptates fuga quos repellat soluta, aliquam, minima iste ullam odit voluptatum ad provident praesentium reprehenderit dolore. Corporis laborum omnis possimus alias, facere deleniti aspernatur!
-        Quibusdam, dignissimos quas iusto fugiat numquam, error ipsam maxime, enim consectetur labore aliquid quam sint cupiditate fugit. Perferendis unde numquam, architecto iste rem saepe ullam earum ducimus deserunt! Molestiae, sint.
-        Neque ad dolorum sunt repellat aperiam eveniet eos quia rem minus, adipisci deserunt officiis! Quaerat ad animi, vitae corrupti explicabo cumque perspiciatis sed temporibus doloribus cum. Exercitationem ratione labore sequi.
-        Earum, rerum praesentium a ea minima neque magni officiis est molestias delectus libero! Earum provident repellendus soluta ex nihil quam debitis expedita. Perspiciatis dolore officia omnis, eaque ratione voluptatem iure.
-        Veritatis reprehenderit porro quae dolorum velit illo laboriosam accusamus veniam voluptas non debitis, aliquid, rerum accusantium, libero cumque officiis. Distinctio blanditiis cumque soluta quod quae quidem neque quos. Quo, totam.
-        Quod cupiditate itaque esse dicta omnis sint odit sunt ad dolorum tenetur modi perferendis asperiores porro, praesentium eligendi repellat autem incidunt. Quae itaque omnis laudantium totam, laboriosam amet aliquid doloremque.
-        Asperiores incidunt officiis consectetur consequuntur natus accusantium odit maxime ducimus iusto totam dolore, quos recusandae expedita voluptatibus! Hic quia iste reiciendis inventore! Iusto, omnis aliquam! Aspernatur quidem pariatur corporis facere.
-        Obcaecati rem accusantium, quasi ea eligendi laboriosam sint delectus animi eius nulla, pariatur optio sed itaque quas corporis voluptatem similique eaque minima? Assumenda veniam et reiciendis omnis necessitatibus ipsam beatae.
-        Magnam, accusamus aut necessitatibus, minus doloribus natus quae magni nesciunt voluptatibus maxime tenetur placeat, voluptates modi dolorum distinctio ad ipsum ut nihil! Ipsa cumque beatae, quis dignissimos tenetur culpa sed.
-        Tenetur provident natus numquam cumque? Delectus aut, distinctio nobis natus quo repudiandae sapiente laborum enim harum doloribus odio autem quidem mollitia accusamus. Eius ea ex, recusandae quos aut quam cupiditate.
-        Magni, quos eum? Dignissimos, accusantium consequatur voluptate quidem impedit consectetur autem ea molestias, id totam facere quia officia quae perferendis praesentium! Distinctio quis ut, nisi totam atque qui? Et, reprehenderit?
-        Eius nulla inventore error voluptatem voluptas sed hic, praesentium laudantium suscipit aliquam doloremque adipisci corrupti debitis facilis dolorum, nihil fugit doloribus eos ad numquam non ex quam quas? Maiores, soluta?
-        Pariatur, minima! In nemo similique voluptatum voluptas error officia labore repudiandae amet tempora nam, adipisci ad asperiores reiciendis velit odit unde necessitatibus atque. Sint sunt fugit possimus! Vel, sequi ab!
-        Iusto, modi exercitationem! Asperiores soluta incidunt maxime minus illum unde temporibus officiis necessitatibus animi rerum, iusto mollitia voluptates ab saepe nulla optio ipsum dicta placeat quod eius illo? Perspiciatis, excepturi.
-        Quam ipsum, id quasi iusto ut sequi possimus aspernatur omnis dolore suscipit? Ipsam natus assumenda harum rem, culpa molestiae? Veritatis, ad earum sapiente rem rerum voluptates corrupti commodi explicabo quidem?
-        Sunt a vero itaque temporibus culpa est nemo recusandae nobis maxime, laudantium, incidunt repellat eligendi facere quaerat voluptatum natus ullam perspiciatis dolor deleniti. Delectus ab tempore similique sunt, molestiae tempora!
-        Porro, obcaecati dolor et tempore facere voluptates placeat sapiente, cupiditate pariatur repellendus amet sequi nam! Recusandae, ducimus molestias consequatur ad atque quasi alias ratione quibusdam? Iure facere quod doloribus accusantium.
-        Minima a assumenda nihil dolorem tempora similique quod eaque, unde modi obcaecati veritatis eveniet cumque quaerat minus nemo quasi. Omnis eius veniam sint. Nam minus soluta distinctio ratione deleniti voluptas?
-        Quibusdam nam quaerat ratione nesciunt. Ea accusantium architecto, error reiciendis tempora ut ducimus odio consectetur rerum at sed iusto ipsa sequi perferendis distinctio beatae veniam quas itaque sunt, nesciunt modi.
-        Id unde, explicabo eum quis expedita ut? Facilis, minima numquam magnam ipsum quae laudantium quis, vitae ex corporis at maxime aut voluptatum, possimus non quod fugiat ullam aliquid iure neque.
-        Ea ratione ducimus, exercitationem necessitatibus dolore consequatur quas architecto autem iure totam nulla modi, magnam ad esse quia dignissimos explicabo animi sequi est, commodi nam delectus quam laborum aperiam? Ipsa.
-        Voluptas quam totam tempore libero, cupiditate necessitatibus unde officia non voluptatem excepturi? Libero, quasi ipsum rem pariatur illum commodi repellendus nam quis ullam quibusdam alias magnam voluptatum a quae mollitia.
-        Dolorum, ad earum debitis voluptatum, minima cupiditate et nobis aliquid quis quos maiores quae esse, accusamus ab nihil! Nulla nobis laudantium dolores sapiente fuga molestiae, nam temporibus ducimus sunt dignissimos?
-        Sequi sint, nostrum deserunt rerum impedit quos dolorum commodi velit beatae! Nobis quaerat voluptates facilis ullam dolore animi vero? Itaque hic qui harum dolore iure sapiente molestias dolorem quam mollitia.
-        Voluptas voluptatibus distinctio deleniti aut, fugit similique? Ad praesentium eos dolor consectetur corporis sapiente, eveniet doloribus vitae nemo adipisci laboriosam est impedit ipsa expedita, suscipit assumenda reiciendis laudantium, autem enim.
-        Nostrum doloribus quas laudantium nam est incidunt suscipit. Est animi vero magnam recusandae ipsum aperiam repudiandae ad veritatis suscipit maiores, cum perspiciatis voluptatum. Ratione ullam quis odit et quas magni.
-        Dicta assumenda illum ea, nam porro animi. Quidem non voluptate omnis adipisci ex earum debitis, asperiores, eius officia rerum vero ad distinctio fugit quam ullam sequi rem placeat perferendis velit?
-        Repellat deserunt veritatis id exercitationem fugit quos illo? Omnis maiores et ipsa quam nam labore accusamus sint adipisci nulla temporibus excepturi, quaerat tempora enim magnam minima consequuntur eaque! Ab, ut?
-        Veritatis deleniti, quasi odit maxime doloremque consequuntur voluptate quae quisquam quod illo. Excepturi, doloremque. Omnis assumenda ullam facere obcaecati deserunt dolores possimus deleniti! At earum eius dicta mollitia eveniet veniam.
-        Ducimus, reiciendis facere. Debitis, optio. Ex, explicabo iste ullam voluptates vitae culpa natus voluptas molestiae architecto quibusdam expedita ratione voluptatem similique alias amet? Praesentium officia veritatis laboriosam id cum nihil!
-        Iusto nobis hic exercitationem praesentium unde odio obcaecati, rem explicabo voluptate ab ex consectetur. Minus tempora, et magnam in adipisci quas iusto fugiat vitae porro voluptatem. Dolore facere cum beatae!
-        Non, placeat natus aliquid est sint libero debitis quod vitae ab iusto ea explicabo consequuntur hic aliquam! Repudiandae temporibus ratione cum illum ex asperiores iste ipsa? Beatae alias tempore tenetur!
-        Eaque voluptate, voluptatem vitae saepe eius modi aspernatur reiciendis? Unde eius suscipit exercitationem ipsa aut? Provident quasi sint maiores distinctio tempora, fuga aliquid impedit aliquam, odit id quia, voluptas ut?
-        Cupiditate blanditiis dolorem ipsa provident. Illum pariatur amet facilis error corporis cupiditate? Alias odio dolore similique quas, sed quo doloremque dolorem iste! Ut optio, perferendis eius reiciendis deleniti doloribus cupiditate?
-        Iusto ex beatae dolor error eos quaerat ipsa ipsam atque dolore natus nesciunt aspernatur quasi maxime, perspiciatis, animi consequatur molestias eligendi iure. Vel, quod. Aliquid officia nemo magni sint temporibus?
-        Beatae officiis consequuntur quod eveniet pariatur officia ipsa quis exercitationem. Explicabo cupiditate, saepe nisi dolor quam id fugiat, optio deserunt facilis provident ipsam voluptates numquam ratione laudantium qui incidunt dignissimos!
-        Fuga deserunt et rerum, necessitatibus libero eveniet tempora quibusdam quia labore odit qui ipsam aliquid facere ipsa animi unde repudiandae iure mollitia voluptates! Obcaecati beatae, animi doloremque ratione dolores culpa!
-        Sed hic provident molestias consectetur doloremque impedit natus exercitationem labore est, consequuntur facilis et quia sunt iste voluptatem quaerat dicta blanditiis reiciendis ab? Aperiam numquam minima quos nihil atque inventore.
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Quizzes Page</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600;1,700&family=Inter:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Cardo:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/main.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Quizzes Page
+  * Author: Your Name
+  * License: Your License
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="header d-flex align-items-center fixed-top">
+    <div class="container-fluid d-flex align-items-center justify-content-between">
+
+      <a href="index.html" class="logo d-flex align-items-center  me-auto me-lg-0">
+        <i class="bi bi-pencil-square"></i>
+        <h1>Quizzes</h1>
+      </a>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a href="index.html" class="active">Home</a></li>
+          <li><a href="about.html">About</a></li>
+          <li class="dropdown"><a href="#"><span>Quizzes</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+            <ul>
+              <li><a href="#">Math</a></li>
+              <li><a href="#">Science</a></li>
+              <li><a href="#">History</a></li>
+              <li><a href="#">Geography</a></li>
+              <li><a href="#">Language</a></li>
+            </ul>
+          </li>
+          <li><a href="services.html">Services</a></li>
+          <li><a href="contact.html">Contact</a></li>
+        </ul>
+      </nav><!-- .navbar -->
+
+      <div class="header-social-links">
+        <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+        <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+        <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+        <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></i></a>
+      </div>
+      <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
+      <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
+
     </div>
-@endsection
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="hero d-flex flex-column justify-content-center align-items-center" data-aos="fade" data-aos-delay="1500">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-6 text-center">
+          <h2>Welcome to Our Quizzes Section</h2>
+          <p>Test your knowledge with our quizzes in various subjects!</p>
+          <a href="#" class="btn-get-started">Take a Quiz</a>
+        </div>
+      </div>
+    </div>
+  </section><!-- End Hero Section -->
+
+  <main id="main" data-aos="fade" data-aos-delay="1500">
+
+    <!-- ======= Quiz Section ======= -->
+    <section id="quiz" class="quiz">
+      <div class="container-fluid">
+
+        <div class="row gy-4 justify-content-center">
+          <!-- Quiz Cards will go here -->
+        </div>
+
+      </div>
+    </section><!-- End Quiz Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer" class="footer">
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong><span>Quizzes</span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        Designed by <a href="https://yourwebsite.com/">Your Name</a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <div id="preloader">
+    <div class="line"></div>
+  </div>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
