85c84b4c78ffdf30c3b05b08c3a35ca85d85c273
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-04-09 20:22:07 +0000

    Update HomeController.php to include categories and published quizzes in the home view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Quize;
 use Illuminate\Http\Request;
 
@@ -10,6 +11,8 @@ class HomeController extends Controller
     public function index()
     {
         $quizzes = Quize::all();
-        return view('home', compact('quizzes'));
+        $categories = Category::all();
+        $quizzes = Quize::where('status', 'published')->get();
+        return view('home', compact('quizzes', 'categories'));
     }   
 }
