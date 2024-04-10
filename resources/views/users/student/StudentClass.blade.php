db6a9688cf4db779724fb0887524b4d1d2a9f2f0
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-04-10 20:51:32 +0000

    Update link for My Class Quizzes in StudentClass.blade.php

--- resources/views/users/student/StudentClass.blade.php
+++ resources/views/users/student/StudentClass.blade.php
@@ -170,7 +170,7 @@
                     <div class="col-md-5 align-self-center">
                         <h3 class="text-themecolor">Profile</h3>
                         <ol class="breadcrumb">
-                            <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
+                            <li class="breadcrumb-item"><a href="{{route('home')}}">Home</a></li>
                             <li class="breadcrumb-item active">Profile</li>
                         </ol>
                     </div>
