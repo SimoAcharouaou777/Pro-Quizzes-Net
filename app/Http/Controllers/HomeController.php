4daf642bc576289aa122142a9368815cc3bc4f6e
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-04-11 17:44:02 +0000

    Update HomeController.php to include student and representative models in index method

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,15 +4,20 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use App\Models\Quize;
-use Illuminate\Http\Request;
+use App\Models\representative;
+use App\Models\Student;
+
 
 class HomeController extends Controller
 {
     public function index()
     {
+        $user = auth()->user();
+        $student = Student::where('user_id', $user->id)->first();
+        $representative = representative::where('user_id', $user->id)->first();
         $categories = Category::all();
         $quizzes = Quize::where('status', 'published')->with('user')->get();
         
-        return view('home', compact('quizzes', 'categories', 'quizzes'));
+        return view('home', compact('quizzes', 'categories', 'quizzes', 'student', 'representative', 'user'));
     }   
 }
