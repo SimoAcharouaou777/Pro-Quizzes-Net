8d789f41b8f38425aea46b5b792a42a343638d3d
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-04-11 10:16:09 +0000

    Add Spatie MediaLibrary functionality to representative model

--- app/Models/representative.php
+++ app/Models/representative.php
@@ -4,15 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class representative extends Model
+class representative extends Model implements HasMedia
 {
-    use HasFactory;
+    use HasFactory, InteractsWithMedia;
     protected $table = 'representatives';
 
     protected $fillable = [
         'name',
         'user_id',
+        'company_id'
     ];
 
     public function users()
