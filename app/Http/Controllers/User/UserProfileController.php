d8a785ed1125a6da89977c1617ade749a3822c97
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-04-11 10:15:59 +0000

    Refactor UserProfileController.php to include representative model and update user and student views

--- app/Http/Controllers/User/UserProfileController.php
+++ app/Http/Controllers/User/UserProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\representative;
 use App\Models\Student;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -16,7 +17,8 @@ class UserProfileController extends Controller
     {
         $user = auth()->user();
         $student = Student::where('user_id', $user->id)->first();
-        return view('users.UserProfile', compact('user', 'student'));
+        $representative = representative::where('user_id', $user->id)->first();
+        return view('users.UserProfile', compact('user', 'student','representative'));
     }
 
     /**
@@ -58,6 +60,7 @@ class UserProfileController extends Controller
     {
         $user = User::find($id);
         $student = Student::where('user_id', $user->id)->first();
+        $representative = representative::where('user_id', $user->id)->first();
         if ($request->has('username')) {
             $user->username = $request->input('username');
         }
@@ -74,13 +77,38 @@ class UserProfileController extends Controller
             $student->student_id = $request->input('student_id');
             $student->save();
         }
+        if($user->hasRole('representative')){
+            $company = $representative->company;
+            if($request->has('company_name')){
+                $company->company_name = $request->input('company_name');
+            }
+            if($request->has('company_email')){
+                $company->company_email = $request->input('company_email');
+            }
+            if($request->has('description')){
+                $company->description = $request->input('description');
+            }
+            if($request->has('domaine')){
+                $company->domaine = $request->input('domaine');
+            }
+            if($request->has('location')){
+                $company->location = $request->input('location');
+            }
+            if($request->has('founded_date')){
+                $company->founded_date = $request->input('founded_date');
+            }
+            $company->save();
+        }
         if ($request->hasFile('image')) {
             $image = $request->file('image');
             if ($image->isValid()) {
                 if ($user->hasRole('student')) {
                     $student->clearMediaCollection('media/students');
                     $student->addMediaFromRequest('image')->toMediaCollection('media/students', 'media_students');
-                } else {
+                }elseif ($user->hasRole('representative')) {
+                    $representative->clearMediaCollection('media/representatives');
+                    $representative->addMediaFromRequest('image')->toMediaCollection('media/representatives', 'media_representatives');
+                }else{
                     $user->clearMediaCollection('media/users');
                     $user->addMediaFromRequest('image')->toMediaCollection('media/users', 'media_users');
                 }
