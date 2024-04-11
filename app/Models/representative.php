3292430ed3684bf4b6ef5f30b545d5d653c32cb0
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-04-11 17:44:24 +0000

    Update representative model to use singular 'user' relationship

--- app/Models/representative.php
+++ app/Models/representative.php
@@ -18,7 +18,7 @@ class representative extends Model implements HasMedia
         'company_id'
     ];
 
-    public function users()
+    public function user()
     {
         return $this->belongsTo(User::class);
     }
