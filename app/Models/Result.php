45a582a98fe8494f06579e0e7ef3c9d39ae67f76
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-04-13 15:40:33 +0000

    Add $fillable property to Result model in Result.php

--- app/Models/Result.php
+++ app/Models/Result.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Result extends Model
 {
     use HasFactory;
+    
+    protected $fillable = ['user_id', 'quiz_id', 'question_id', 'answer_id'];
+    
+
 }
