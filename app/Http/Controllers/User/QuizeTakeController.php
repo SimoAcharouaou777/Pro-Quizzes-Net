61d2fce241490d5e0d6cb8bc4c566bf7c69bfd62
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-04-13 13:50:41 +0000

    Add QuizSubmit method to QuizeTakeController.php for submitting quiz answers

--- app/Http/Controllers/User/QuizeTakeController.php
+++ app/Http/Controllers/User/QuizeTakeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use App\Models\Quize;
 use Illuminate\Http\Request;
+use App\Models\Result;
 
 class QuizeTakeController extends Controller
 {
@@ -12,5 +13,31 @@ class QuizeTakeController extends Controller
     {
         $quiz = Quize::find($id);
         return view('users.UserQuizeTake',compact('quiz'));
+    }
+
+    public function QuizSubmit(Request $request, $id)
+    {
+        $quiz= Quize::find($id);
+        if($quiz->quiz_type == 'multiple_choice'){
+            $score = 0;
+            foreach ($request->input('answers', []) as $questionId => $answerId) {
+                $question = $quiz->questions()->find($questionId);
+                $answer = $question->answers()->find($answerId);
+    
+                if ($answer->status == 'true') {
+                    $score++;
+                }
+    
+                Result::create([
+                    'user_id' => auth()->id(),
+                    'quiz_id' => $quiz->id,
+                    'question_id' => $questionId,
+                    'answer_id' => $answerId,
+                ]);
+            }
+    
+            return redirect()->route('home')->with('message', 'Your results can be found in your quizzes results');
+        }
+        
     }
 }
