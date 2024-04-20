35d3c1b32b1c349dfe4c429d55eada92a8e1b83a
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-04-20 19:42:54 +0100

    Refactor MyResults.blade.php for improved readability and removal of unnecessary code

--- resources/views/users/UserDashboard.blade.php
+++ resources/views/users/UserDashboard.blade.php
@@ -147,7 +147,7 @@
                 <!-- Sidebar navigation-->
                 <nav class="sidebar-nav">
                     <ul id="sidebarnav">
-                        <li> <a class="waves-effect waves-dark" href="index.html" aria-expanded="false"><i
+                        <li> <a class="waves-effect waves-dark" href="{{route('userdashboard.index')}}" aria-expanded="false"><i
                                     class="fa fa-tachometer"></i><span class="hide-menu">Dashboard</span></a>
                         </li>
                         <li> <a class="waves-effect waves-dark" href="{{route('userprofile.index')}}" aria-expanded="false"><i
@@ -156,9 +156,11 @@
                         <li> <a class="waves-effect waves-dark" href="{{route('usersettings.index')}}" aria-expanded="false"><i
                                     class="fa fa-table"></i><span class="hide-menu">Settings</span></a>
                         </li>
+                        @if($user->hasRole('teacher') || $user->hasRole('representative'))
                         <li> <a class="waves-effect waves-dark" href="{{route('userquizzes.index')}}" aria-expanded="false"><i
                                     class="fa fa-smile-o"></i><span class="hide-menu">Quizzes</span></a>
                         </li>
+                        @endif
                         <li> 
                             @if(Auth::user()->hasRole('teacher'))
                             <a class="waves-effect waves-dark" href="{{route('teacherClass')}}" aria-expanded="false"><i
@@ -209,37 +211,14 @@
                 <!-- ============================================================== -->
                 <div class="row">
                     <!-- Column -->
-                    <div class="col-lg-8">
-                        <div class="card">
-                            <div class="card-body">
-                                <div class="d-flex no-block">
-                                    <div>
-                                        <h5 class="card-title mb-0">Sales Chart</h5>
-                                    </div>
-                                    <div class="ms-auto">
-                                        <ul class="list-inline text-center font-12">
-                                            <li><i class="fa fa-circle text-success"></i> SITE A</li>
-                                            <li><i class="fa fa-circle text-info"></i> SITE B</li>
-                                            <li><i class="fa fa-circle text-primary"></i> SITE C</li>
-                                        </ul>
-                                    </div>
-                                </div>
-                                <div class="" id="sales-chart" style="height: 355px;"></div>
-                            </div>
-                        </div>
-                    </div>
+
+                    
                     <!-- Column -->
                     <div class="col-lg-4">
                         <div class="card">
                             <div class="card-body">
                                 <div class="d-flex mb-4 no-block">
                                     <h5 class="card-title mb-0 align-self-center">Our Visitors</h5>
-                                    <div class="ms-auto">
-                                        <select class="form-select b-0">
-                                            <option selected="">Today</option>
-                                            <option value="1">Tomorrow</option>
-                                        </select>
-                                    </div>
                                 </div>
                                 <div id="visitor" style="height:260px; width:100%;"></div>
                                 <ul class="list-inline mt-4 text-center font-12">
@@ -266,14 +245,6 @@
                                     <div>
                                         <h5 class="card-title">My Quizzes</h5>
                                     </div>
-                                    <div class="ms-auto">
-                                        <select class="form-select b-0">
-                                            <option selected="">January</option>
-                                            <option value="1">February</option>
-                                            <option value="2">March</option>
-                                            <option value="3">April</option>
-                                        </select>
-                                    </div>
                                 </div>
                                 <div class="table-responsive mt-3 no-wrap">
                                     <table class="table vm no-th-brd pro-of-month">
@@ -285,47 +256,20 @@
                                             </tr>
                                         </thead>
                                         <tbody>
+                                            @foreach($quizzes as $quiz)
                                             <tr>
-                                                <td style="width:50px;"><span class="round">S</span></td>
+                                                @if($quiz->hasMedia('media/quizzes'))
+                                                <td style="width:50px;"><img src="{{$quiz->getFirstMediaUrl('media/quizzes')}}" alt="user" class="img-circle" width="50"></td>
+                                                @else
+                                                <td style="width:50px;"><span class="round">{{substr($quiz->title, 0, 1)}}</span></td>
+                                                @endif
                                                 <td>
-                                                    <h6>Quizze Name</h6><small class="text-muted">Quizze Type</small>
+                                                    <h6>{{$quiz->title}}</h6><small class="text-muted">{{$quiz->quiz_type}}</small>
                                                 </td>
-                                                <td>Elite Admin</td>
-                                                <td>$3.9K</td>
+                                                <td>{{$quiz->created_at->format('M d, Y')}}</td>
+                                                <td>{{$participants}}</td>
                                             </tr>
-                                            <tr class="active">
-                                                <td><span class="round"><img src="../assets/images/users/2.jpg"
-                                                            alt="user" width="50"></span></td>
-                                                <td>
-                                                    <h6>Andrew</h6><small class="text-muted">Project Manager</small>
-                                                </td>
-                                                <td>Real Homes</td>
-                                                <td>$23.9K</td>
-                                            </tr>
-                                            <tr>
-                                                <td><span class="round round-success">B</span></td>
-                                                <td>
-                                                    <h6>Bhavesh patel</h6><small class="text-muted">Developer</small>
-                                                </td>
-                                                <td>MedicalPro Theme</td>
-                                                <td>$12.9K</td>
-                                            </tr>
-                                            <tr>
-                                                <td><span class="round round-primary">N</span></td>
-                                                <td>
-                                                    <h6>Nirav Joshi</h6><small class="text-muted">Frontend Eng</small>
-                                                </td>
-                                                <td>Elite Admin</td>
-                                                <td>$10.9K</td>
-                                            </tr>
-                                            <tr>
-                                                <td><span class="round round-warning">M</span></td>
-                                                <td>
-                                                    <h6>Micheal Doe</h6><small class="text-muted">Content Writer</small>
-                                                </td>
-                                                <td>Helping Hands</td>
-                                                <td>$12.9K</td>
-                                            </tr>
+                                            @endforeach
                                         </tbody>
                                     </table>
                                 </div>
@@ -336,21 +280,16 @@
                     <!-- Column -->
                     <div class="col-lg-4 d-flex align-items-stretch">
                         <div class="card w-100">
-                            <div class="up-img" style="background-image:url(../assets/images/big/img1.jpg)"></div>
+                            @if($mostParticipatedQuiz->hasMedia('media/quizzes'))
+                            <div  class="card-img-top" >
+                                <img src="{{ $mostParticipatedQuiz->getFirstMedia('media/quizzes')->getUrl() }}" alt="Quiz Image" style="width: 100%; height:200px; object-fit: cover;">
+                            </div>
+                            @endif
+                            
                             <div class="card-body">
-                                <h5 class=" card-title">Business development of rules</h5>
-                                <span class="label label-info label-rounded">Technology</span>
-                                <p class="mb-0 mt-3">Titudin venenatis ipsum aciat. Vestibu ullamer quam. nenatis
-                                    ipsum ac feugiat. Ibulum ullamcorper.</p>
-                                <div class="d-flex mt-3">
-                                    <a class="link" href="javascript:void(0)">Read more</a>
-                                    <div class="ms-auto align-self-center">
-                                        <a href="javascript:void(0)" class="link me-2"><i
-                                                class="fa fa-heart-o"></i></a>
-                                        <a href="javascript:void(0)" class="link me-2"><i
-                                                class="fa fa-share-alt"></i></a>
-                                    </div>
-                                </div>
+                                <h5 class=" card-title">{{$mostParticipatedQuiz->title}}</h5>
+                                <span class="label label-info label-rounded">{{$mostParticipatedQuiz->category->name}}</span>
+                                <p class="mb-0 mt-3">{{$mostParticipatedQuiz->description}}</p>
                             </div>
                         </div>
                     </div>
