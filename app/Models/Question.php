8bb0177c806b01e2622a1c0a9786156355ada94b
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-04-20 19:42:25 +0100

    Add results relationship to Question model

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -18,5 +18,9 @@ class Question extends Model
     {
         return $this->hasMany(Answer::class);
     }
+
+    public function results(){
+        return $this->hasMany(Result::class);
+    }
     
 }
