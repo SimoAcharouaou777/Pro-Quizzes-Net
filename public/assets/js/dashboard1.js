29b7ad97036489efd0312dd1df83949fb22b59d6
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-04-20 19:42:36 +0100

    Update dashboard1.js to use AJAX to fetch chart data from API

--- public/assets/js/dashboard1.js
+++ public/assets/js/dashboard1.js
@@ -9,38 +9,33 @@ $(function() {
     // ============================================================== 
     // Our Visitor
     // ============================================================== 
-
-    var chart = c3.generate({
-        bindto: '#visitor',
-        data: {
-            columns: [
-                ['Other', 30],
-                ['Desktop', 10],
-                ['Tablet', 40],
-                ['Mobile', 50],
-            ],
-
-            type: 'donut',
-            onclick: function(d, i) { console.log("onclick", d, i); },
-            onmouseover: function(d, i) { console.log("onmouseover", d, i); },
-            onmouseout: function(d, i) { console.log("onmouseout", d, i); }
-        },
-        donut: {
-            label: {
-                show: false
-            },
-            title: "Visits",
-            width: 20,
-
-        },
-
-        legend: {
-            hide: true
-            //or hide: 'data1'
-            //or hide: ['data1', 'data2']
-        },
-        color: {
-            pattern: ['#eceff1', '#24d2b5', '#6772e5', '#20aee3']
+    $.ajax({
+        url: 'http://127.0.0.1:8000/getChartStatics', // Replace with the URL of your API
+        method: 'GET',
+        success: function(data) {
+            var chart = c3.generate({
+                bindto: '#visitor',
+                data: {
+                    columns: data,
+                    type: 'bar',
+                    onclick: function(d, i) { console.log("onclick", d, i); },
+                    onmouseover: function(d, i) { console.log("onmouseover", d, i); },
+                    onmouseout: function(d, i) { console.log("onmouseout", d, i); }
+                },
+                donut: {
+                    label: {
+                        show: false
+                    },
+                    title: "Visits",
+                    width: 20,  
+                },
+                legend: {
+                    hide: true
+                },
+                color: {
+                    pattern: ['#eceff1', '#24d2b5', '#6772e5', '#20aee3']
+                }
+            });
         }
     });
     // ============================================================== 
