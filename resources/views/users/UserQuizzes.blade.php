a4c5ed5c90137cbba902f0856e976d5e044b64ad
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-04-23 16:51:16 +0100

    Update logo images in UserQuizzes.blade.php

--- resources/views/users/UserQuizzes.blade.php
+++ resources/views/users/UserQuizzes.blade.php
@@ -56,16 +56,16 @@
                         <!-- Logo icon --><b>
                             <!--You can put here icon as well // <i class="wi wi-sunset"></i> //-->
                             <!-- Dark Logo icon -->
-                            <img src="{{asset('assets/images/logo-icon.png')}}" alt="homepage" class="dark-logo" />
+                            <img src="" alt="" class="dark-logo" />
                             <!-- Light Logo icon -->
-                            <img src="{{asset('assets/images/logo-light-icon.png')}}" alt="homepage" class="light-logo" />
+                            <img src="" alt="" class="light-logo" />
                         </b>
-                        <!--End Logo icon -->
-                        <!-- Logo text --><span>
+                        <span>
                             <!-- dark Logo text -->
-                            <img src="{{asset('assets/images/logo-text.png')}}" alt="homepage" class="dark-logo" />
+                            <img src="" alt="" class="dark-logo" />
                             <!-- Light Logo text -->
-                            <img src="{{asset('assets/images/logo-light-text.png')}}" class="light-logo" alt="homepage" /></span>
+                            <img src="" class="light-logo" alt="" />
+                        </span>
                     </a>
                 </div>
                 <!-- ============================================================== -->
