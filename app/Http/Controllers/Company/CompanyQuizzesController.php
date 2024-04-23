20d695eb2b817c72d292e97ed0bfdb42faea7b6c
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-04-23 16:49:17 +0100

    Add authorization check for admin users in CompanyQuizzesController.php

--- app/Http/Controllers/Company/CompanyQuizzesController.php
+++ app/Http/Controllers/Company/CompanyQuizzesController.php
@@ -18,6 +18,9 @@ class CompanyQuizzesController extends Controller
     {
         $categories = Category::all();
         $user = auth()->user();
+        if($user->hasRole('admin')){
+            return redirect()->back()->with('error', 'You are not authorized to view this page.');
+        }
         $student = Student::where('user_id', $user->id)->first();
         $representative = representative::where('user_id', $user->id)->first();
         $quizzes = Quize::whereHas('user', function($query){
