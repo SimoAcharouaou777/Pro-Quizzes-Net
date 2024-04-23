30c5d24e186972f2882a88dbcc645084a5bffc0f
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-04-23 16:51:07 +0100

    Update logo images in UserProfile.blade.php

--- resources/views/users/UserProfile.blade.php
+++ resources/views/users/UserProfile.blade.php
@@ -52,20 +52,20 @@
                 <!-- Logo -->
                 
                 <div class="navbar-header">
-                    <a class="navbar-brand" href="index.html">
+                    <a class="navbar-brand" href="">
                         <!-- Logo icon --><b>
                             <!--You can put here icon as well // <i class="wi wi-sunset"></i> //-->
                             <!-- Dark Logo icon -->
-                            <img src="{{asset('assets/images/logo-icon.png')}}" alt="homepage" class="dark-logo" />
+                            <img src="" alt="" class="dark-logo" />
                             <!-- Light Logo icon -->
-                            <img src="{{asset('assets/images/logo-light-icon.png')}}" alt="homepage" class="light-logo" />
+                            <img src="" alt="" class="light-logo" />
                         </b>
                         <!--End Logo icon -->
                         <!-- Logo text --><span>
                             <!-- dark Logo text -->
-                            <img src="{{asset('assets/images/logo-text.png')}}" alt="homepage" class="dark-logo" />
+                            <img src="" alt="" class="dark-logo" />
                             <!-- Light Logo text -->
-                            <img src="{{asset('assets/images/logo-light-text.png')}}" class="light-logo" alt="homepage" /></span>
+                            <img src="" class="light-logo" alt="" /></span>
                     </a>
                 </div>
                 
