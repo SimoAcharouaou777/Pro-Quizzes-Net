c81aa86d3902f775a3213818c7cdf9237d153b36
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-04-23 12:07:34 +0100

    Add status field to students table in create_students_table migration

--- database/migrations/2024_02_23_142307_create_students_table.php
+++ database/migrations/2024_02_23_142307_create_students_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email');
             $table->string('phone_number')->nullable();
             $table->string('student_id')->nullable();
+            $table->enum('status',['active', 'banned'])->default('active');
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade')->onUpdate('cascade');
             $table->timestamps();
         });
